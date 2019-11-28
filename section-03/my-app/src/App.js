aae598cdfabd14dc69010f2137507a9a3fe8fbeb
jamesvrooney <jamesvrooney@hotmail.com>
2019-11-28 18:51:52 +0000

    Created a simple functional comonent.

--- section-03/my-app/src/App.js
+++ section-03/my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Person from './Person/Person'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hi, I'm a React developer.</h1>
+      <Person/>
+      <Person/>
+      <Person/>
     </div>
   );
 }
